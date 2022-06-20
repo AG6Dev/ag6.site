0f9c294e21a29cc41403d091a97053e8fba38b94
AG6 <60290746+AG6Dev@users.noreply.github.com>
2022-06-20 16:27:33 +0100

    work on about section a bit more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import icon from "../images/icon.png";
-import Typewriter from "typewriter-effect";
 
 import "../styles/about.css";
+import Typewriter from "typewriter-effect";
 
 export default function About() {
     return (
         <div className="about">
-            <img src={icon} alt="AG6"/>
-            <h1>Hello!</h1>
+            <div className="about-image">
+                <img src={icon} alt="AG6 Logo"/>
+            </div>
+            <div className="about-info">
+                <h1>Hello!</h1>
 
-            <Typewriter options={{
-                strings: ["Java Developer",
-                    "Python Developer",
-                    "Backend Developer",
-                    "Web Developer"],
-                autoStart: true,
-                loop: true,
+                <span className="about-info-intro">I am a 16 year old
+                <Typewriter options={{
+                    strings: [" Java Developer",
+                        " Python Developer",
+                        " Minecraft Modder"],
+                    autoStart: true,
+                    loop: true,
+                    typeSpeed: 300,
 
-            }} className="about-typewriter"/>
+                }} className="about-typewriter"/>
+                from the UK. </span>
+            </div>
         </div>
     )
 }
