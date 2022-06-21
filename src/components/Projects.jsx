f8c8eee8ea22767f36ae82ac48a43e2a077d99bd
AG6 <60290746+AG6Dev@users.noreply.github.com>
2022-06-21 22:51:49 +0100

    adding annotation generation to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ProjectEntry from "./ProjectEntry";
 import react_icon from "../images/react-icon.svg";
+import annotation_icon from "../images/email-annotation-icon.png";
 
 import "../styles/projects.css";
 
 export default function Projects() {
     return (
-        <a id="projects">
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
+        <a id="projects"    >
             <section className="projects">
                 <span className="projects-title">Projects</span>
                 <div className="projects-div">
                     <ProjectEntry title="AG6.site" description="The site you're currently on! Currently a work in progress but slowly getting there." language="JavaScript + CSS" image={react_icon} github={"https://github.com/AG6Dev/ag6.site/master"}/>
+                    <ProjectEntry title="Annotation Generation" description="An easy way to generate data files using annotations while modding." language="Java" image={annotation_icon} github={"https://github.com/AG6Dev/AnnotationGeneration"}/>
                 </div>
             </section>
         </a>
