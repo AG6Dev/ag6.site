f981c023fdf17bc30d12894b09c0e71996291eea
AG6 <60290746+AG6Dev@users.noreply.github.com>
2022-07-04 00:00:08 +0100

    fix

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Projects from "../components/Projects";
+
+export default function Home() {
+    return (
+        <div>
+            <Navbar/>
+            <About/>
+            <Projects/>
+        </div>
+    )
+}
